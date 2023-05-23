ca053ac564af7e10764726152dc8ddcecee8d399
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-23 13:57:47 +0700

    fix snackbar handling + add setTitle, setPage, setCSS, and removeCSS

--- public/spa.js
+++ public/spa.js
@@ -1,12 +1,16 @@
 const app = document.getElementById('app');
 
-window.addEventListener('locationchange', function () {
-  console.log('location changed!');
-});
+setCSS('global', `
+  html, body, #app {
+    height: 100%;
+    min-height: 100%;
+  }
+`);
 
-init();
+initSnackbar();
+initPage();
 
-async function init () {
+async function initPage () {
   const isLogin = localStorage.getItem('token');
 
   if (isLogin) {
@@ -17,8 +21,33 @@ async function init () {
 }
 
 function loginPage () {
-  document.title = 'Login';
-  app.innerHTML = `
+  setTitle('Login');
+
+  setCSS('login-page', `
+  .login-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+  
+  .login-form {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 400px;
+  }
+  
+  .login-form h1 {
+    align-self: center;
+  }
+  
+  .login-form input {
+    margin: 10px 0;
+  }
+  `);
+
+  setPage(`
   <div class="login-page">
     <form class="login-form">
       <h1>Login</h1>
@@ -34,7 +63,7 @@ function loginPage () {
       <button class="register-button" type="button">Register</button>
     </form>
   </div>
-  `;
+  `);
 
   const loginForm = document.querySelector('.login-form');
   const registerButton = document.querySelector('.register-button');
@@ -65,6 +94,7 @@ function loginPage () {
         document.getElementById('login-username').value = '';
         document.getElementById('login-password').value = '';
 
+        removeCSS('login-page');
         profilePage();
       }  
     } catch (e) {
@@ -72,12 +102,40 @@ function loginPage () {
     }
   };
 
-  registerButton.onclick = registerPage;
+  registerButton.onclick = () => {
+    removeCSS('login-page');
+    registerPage();
+  };
 }
 
 function registerPage() {
-  document.title = 'Register';
-  app.innerHTML = `
+  setTitle('Register');
+
+  setCSS('register-page', `
+  .register-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+  
+  .register-form {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 400px;
+  }
+  
+  .register-form h1 {
+    align-self: center;
+  }
+  
+  .register-form input {
+    margin: 10px 0;
+  }
+  `);
+
+  setPage(`
   <div class="register-page">
     <form class="register-form">
       <h1>Register</h1>
@@ -105,7 +163,7 @@ function registerPage() {
       <button class="login-button" type="button">Login</button>
     </form>
   </div>
-  `;
+  `);
 
   const registerForm = document.querySelector('.register-form');
   const loginButton = document.querySelector('.login-button');
@@ -140,6 +198,7 @@ function registerPage() {
         document.getElementById('register-username').value = '';
         document.getElementById('register-password').value = '';
 
+        removeCSS('register-page');
         loginPage();
       }
     } catch (e) {
@@ -147,30 +206,143 @@ function registerPage() {
     }
   }
 
-  loginButton.onclick = loginPage;
+  loginButton.onclick = () => {
+    removeCSS('register-page');
+    loginPage();
+  };
 }
 
 async function profilePage () {
-  document.title = 'Profile';
-  app.innerHTML = `
-    <div>INI ADALAH PROFILE PAGE</div>
-  `;
+  setTitle('Profile');
+
+  setCSS('profile-page', ``);
+  
+  await setPage({
+    page: 
+      layoutComponent(
+        `<div class="profile-page">
+          <div class="profile-image-wrapper">
+            <img src="" alt="">
+          </div>
+          <h1>Nama</h1>
+          <h2>Deskripsi</h2>
+          <h3>Peran</h3>
+          <button class="profile-logout-button">Logout</button>
+        </div>
+        `
+      ),
+    loading: `<div>LOADING BOSS</div>`,
+    isReady: () => new Promise(resolve => setTimeout(() => resolve(true), 3000)) 
+  });
+
+  const logoutButton = document.querySelector('.profile-logout-button');
+
+  logoutButton.onclick = () => {
+    localStorage.removeItem('token');
+    removeCSS('profile-page');
+    removeCSS('layout-component');
+    loginPage();
+  }
+}
+
+function layoutComponent (child) {
+  setCSS('layout-component', `
+  `);
+
+  return `<div class="layout">
+  <div class="layout-header"></div>
+  <div class="layout-main">
+    <div class="layout-sidebar">
+      <h1>User Management</h1>
+      <div class="layout-nav">
+        <button>Profile</button>
+        <button>Users</button>
+        <button>Roles</button>
+        <button>Permissions</button>
+      </div>
+    </div>
+    <div>
+      ${child}
+    </div>
+  </div>
+</div>`
+}
+
+function initSnackbar () {
+  setCSS('snackbar-component', `
+  .snackbar-layout {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    position: fixed;
+    bottom: 50px;
+  }
+  
+  .snackbar-success {
+    margin: 10px;
+    padding: 20px;
+    background-color: rgb(51, 51, 51);
+    color: white;
+  }
+  
+  .snackbar-error {
+    margin: 10px;
+    padding: 20px;
+    background-color: rgb(124, 10, 10);
+    color: white;
+  }
+  `);
+
+  const snackbarLayout = document.createElement('div');
+  snackbarLayout.classList.add('snackbar-layout');
+  document.body.append(snackbarLayout);
 }
 
 function showSnackbar (type, message, duration) {
   const snackbarSuccess = (message) => `<p class="snackbar-success">${message}</p>`;
   const snackbarError = (message) => `<p class="snackbar-error">${message}</p>`;
 
-  const layout = document.createElement('div');
-  layout.classList.add('snackbar-layout');
-  
+  const snackbarLayout = document.body.querySelector('.snackbar-layout');
+
   const snackbar = type === 'error' ? snackbarError : snackbarSuccess;
 
-  layout.innerHTML = snackbar(message);
+  const wrapper = document.createElement('div');
 
-  app.append(layout);
+  wrapper.innerHTML = snackbar(message);
+
+  snackbarLayout.append(wrapper);
 
   setTimeout(() => {
-    layout.remove();
-  }, duration)
+    wrapper.remove();
+  }, duration);
+}
+
+function setTitle (title) {
+  document.title = title;
+}
+
+function setCSS (name, rulesets) {
+  const style = document.createElement('style');
+  style.classList.add(`${name}-styles`);
+  style.innerHTML = rulesets;
+  document.head.append(style);
+}
+
+function removeCSS (name) {
+  const style = document.head.querySelector(`.${name}-styles`);
+
+  style?.remove();
+}
+
+async function setPage (pageOption) {
+  if (typeof pageOption === 'string') {
+    app.innerHTML = pageOption;
+  } else {
+    app.innerHTML = pageOption.loading;
+
+    if (await pageOption.isReady()) {
+      app.innerHTML = pageOption.page;
+    }
+  }
 }
