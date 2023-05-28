49ea36874edcca3b8d6eff6885c0234346a92ed8
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 15:03:38 +0700

    app: utils modular

---
+++ public/utils.js
@@ -0,0 +1,35 @@
+import { errorPage } from '../pages/index.js';
+
+export function setTitle (title) {
+  document.title = title;
+}
+export function setCSS (name, rulesets) {
+  const style = document.createElement('style');
+  style.classList.add(`${name}-styles`);
+  style.innerHTML = rulesets;
+  document.head.append(style);
+}
+export function removeCSS (name) {
+  const style = document.head.querySelector(`.${name}-styles`);
+
+  style?.remove();
+}
+
+export async function setPage (pageOption) {
+  const app = document.getElementById('app');
+
+  if (typeof pageOption === 'string') {
+    app.innerHTML = pageOption;
+  } else {
+    app.innerHTML = pageOption.loading;
+
+    const res = await pageOption.onLoad();
+
+    if (res.isReady) {
+      app.innerHTML = pageOption.page(res.data);
+    } else {
+      removeCSS('header-sidebar-layout');
+      app.innerHTML = errorPage(res.status, res.message, res.data.page)
+    }
+  }
+}
