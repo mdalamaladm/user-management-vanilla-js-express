f3a0327539bc131b3be040f5dcf2f573f0b6e083
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 22:59:57 +0700

    spa: fix body margin + execute dialog init

--- public/spa.js
+++ public/spa.js
@@ -1,15 +1,17 @@
-import { snackbarComponent } from "./components/index.js";
+import { dialogComponent, snackbarComponent } from "./components/index.js";
 import { loginPage, profilePage } from "./pages/index.js";
 import { setCSS } from "./utils.js";
 
 setCSS('global', `
   html, body, #app {
+    margin: 0;
     height: 100%;
     min-height: 100%;
   }
 `);
 
 snackbarComponent.init();
+dialogComponent.init();
 initPage();
 
 async function initPage () {
