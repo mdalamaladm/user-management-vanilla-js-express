583a95f320c383359766e038f6254dbbfce29c22
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 21:01:17 +0700

    permissions: add get permissions

--- routers/permissions.js
+++ routers/permissions.js
@@ -10,7 +10,25 @@ permissionsRouter.use(authJWTMiddleware);
 permissionsRouter.use(checkAccessMiddleware('access-permissions'));
 
 permissionsRouter.route('/')
-  .get(async (req, res) => {})
+  .get(async (req, res) => {
+    try {
+      const roles =
+        (await pgPool.query(`SELECT permissions.id, permissions.name FROM permissions`)).rows;
+
+      res.status(200).json({
+        httpCode: 200,
+        code: 'UM0014',
+        message: 'Permissions Fetched',
+        data: { roles }
+      });
+    } catch (e) {
+      res.status(500).json({
+        httpCode: 500,
+        code: 'UMER0014',
+        message: e
+      });
+    }
+  })
 ;
 
 module.exports = permissionsRouter;
