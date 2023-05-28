ec3df665af09cd004d420ced58ed1bc31a3d1e69
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 21:01:41 +0700

    roles: add get roles

--- routers/roles.js
+++ routers/roles.js
@@ -12,7 +12,25 @@ rolesRouter.use(authJWTMiddleware);
 rolesRouter.use(checkAccessMiddleware('access-roles'));
 
 rolesRouter.route('/')
-  .get(async (req, res) => {})
+  .get(async (req, res) => {
+    try {
+      const roles =
+        (await pgPool.query(`SELECT roles.id, roles.name, permissions.id AS permission_id, permissions.name AS permision FROM roles_permissions INNER JOIN roles ON roles_permissions.role_id = roles.id INNER JOIN permissions ON roles_permissions.permission_id = permissions.id`)).rows;
+
+      res.status(200).json({
+        httpCode: 200,
+        code: 'UM0010',
+        message: 'Roles Fetched',
+        data: { roles }
+      });
+    } catch (e) {
+      res.status(500).json({
+        httpCode: 500,
+        code: 'UMER0010',
+        message: e
+      });
+    }
+  })
   .post(async (req, res) => {})
 ;
 
