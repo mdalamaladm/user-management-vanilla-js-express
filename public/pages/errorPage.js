e7ffe43ce10f67cd89a47b52809fa30e201d5aa5
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 20:59:32 +0700

    errorPage: fix error page display

--- public/pages/errorPage.js
+++ public/pages/errorPage.js
@@ -1,51 +1,29 @@
-import { snackbarComponent } from "../components/index.js";
-import { headerSidebarLayout } from "../layouts/index.js";
 import { removeCSS, setCSS, setPage, setTitle } from "../utils.js";
 import { loginPage } from "./loginPage.js";
 import { profilePage } from "./profilePage.js";
 
-export async function errorPage (errorCode, errorMessage, fromPage) {
-  setTitle(`Error ${errorCode}`);
+export function errorPage (errorHttpCode, errorMessage, fromPage) {
+  const isUnauthorized = errorHttpCode === 403 && errorMessage === 'User unauthorized';
+  const isTokenInvalid = errorHttpCode === 403 && errorMessage === 'Token is invalid';
+  const isFromProfilePage = fromPage === 'profile-page';
+
+  setTitle(`Error ${errorHttpCode}`);
 
   setCSS('error-page', ``);
   
-  await setPage({
-    page: () => headerSidebarLayout(`
-      <div class="error-page">
-        <h1>${errorCode}</h1>
-        <h2>${errorMessage}</h2>
-        <button class="back-button">Go To Profile</button>
-        <button class="login-button">Login</button>
-      </div>
-      `
-    ),
-    loading: `<div>LOADING BOSS</div>`,
-    onLoad: async () => {
-      const response = { httpCode: 200 };
-
-      if (response.httpCode >= 400) {
-        snackbarComponent('error', message, 2000);
-
-        return {
-          isReady: false,
-          status: 400
-        };
-      } else {
-        return {
-          isReady: true,
-          status: 200,
-          data: {}
-        }
-      }
-    }
-  });
-
-  headerSidebarLayout.init('error-page');
+  setPage(`
+    <div class="error-page">
+      <h1>${errorHttpCode}</h1>
+      <h2>${errorMessage}</h2>
+      <button class="back-button">Go To Profile</button>
+      <button class="login-button">Login</button>
+    </div>
+  `);
 
   const backButton = document.querySelector('.back-button');
   const loginButton = document.querySelector('.login-button');
 
-  if (fromPage === 'profile-page') {
+  if (isTokenInvalid || (isFromProfilePage && isUnauthorized)) {
     backButton.hidden = true;
   } else {
     loginButton.hidden = true;
