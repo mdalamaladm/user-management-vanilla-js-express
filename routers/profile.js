9c612826b2ddc319db915fce327a3ebc44c08465
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 22:59:15 +0700

    profiler routers: add put and delete profile

--- routers/profile.js
+++ routers/profile.js
@@ -33,30 +33,62 @@ profileRouter.route('/')
     }
   })
   .put(
-    requiredBodyMiddleware('UMER'),
+    requiredBodyMiddleware('UMER004', ['name', 'username']),
     async (req, res) => {
       try {
         const { userId } = req.user;
 
+        if (!req.body.password) delete req.body.password;
+
+        let setValues = []
+
+        for (const name in req.body) {
+          if (name === 'password') {
+            setValues.push(`${name} = ${await bcrypt.hash(req.body[name], 10)}`);
+          } else {
+            setValues.push(`${name} = ${req.body[name]}`);
+          }
+        }
+
         const profile =
-          (await pgPool.query(`SELECT users.id, users.name, users.description, users.photo, roles.name AS role FROM users INNER JOIN roles ON users.role_id = roles.id WHERE users.id = '${userId}'`)).rows[0];
+          (await pgPool.query(`UPDATE users SET ${setValues.join(',')} WHERE id = '${userId}'`));
 
         res.status(200).json({
           httpCode: 200,
-          code: 'UM003',
-          message: 'Profile Fetched',
+          code: 'UM004',
+          message: 'Profile Updated',
           data: { profile }
         });
       } catch (e) {
         res.status(500).json({
           httpCode: 500,
-          code: 'UMER003',
+          code: 'UMER004',
           message: e
         });
       }
     }
   )
-  .delete(async (req, res) =>{})
+  .delete(async (req, res) => {
+    try {
+      const { userId } = req.user;
+
+      const profile =
+        (await pgPool.query(`REMOVE FROM users WHERE id = '${userId}'`));
+
+      res.status(200).json({
+        httpCode: 200,
+        code: 'UM005',
+        message: 'Profile Deleted',
+        data: { profile }
+      });
+    } catch (e) {
+      res.status(500).json({
+        httpCode: 500,
+        code: 'UMER005',
+        message: e
+      });
+    }
+  })
 ;
 
 module.exports = profileRouter;
