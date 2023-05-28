e96395583715a436c612b95c4adef382a5ed9d03
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-05-28 21:02:36 +0700

    rolesPage: implement get roles

--- public/pages/rolesPage.js
+++ public/pages/rolesPage.js
@@ -8,28 +8,42 @@ export async function rolesPage () {
   setCSS('roles-page', ``);
   
   await setPage({
-    page: () => headerSidebarLayout(
+    page: (data) => headerSidebarLayout(
       `<div class="roles-page">
-        ROLES PAGE
+        ${JSON.stringify(data)}
       </div>
       `
     ),
     loading: `<div>LOADING BOSS</div>`,
     onLoad: async () => {
-      const response = { httpCode: 200 };
+      const token = localStorage.getItem('token');
+
+      const rawResponse = await fetch('http://localhost:5000/roles', {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      const response = await rawResponse.json();
+      
+      const message = `${response.code} - ${response.message}`;
 
       if (response.httpCode >= 400) {
         snackbarComponent('error', message, 2000);
 
         return {
           isReady: false,
-          status: 400
+          httpCode: response.httpCode,
+          code: response.code,
+          message: response.message,
+          data: { page: 'roles-page' },
         };
       } else {
         return {
           isReady: true,
-          status: 200,
-          data: {}
+          httpCode: response.httpCode,
+          code: response.code,
+          message: response.message,
+          data: response.data,
         }
       }
     }
