f50f0b7a13317ca172cb48fae51aab0760383ba9
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-04 16:49:10 +0700

    feat(components): register table component

--- public/components/index.js
+++ public/components/index.js
@@ -1,3 +1,4 @@
 export { snackbarComponent } from './snackbarComponent.js';
 export { dialogComponent } from './dialogComponent.js';
 export { formComponent } from './formComponent.js';
+export { tableComponent } from './tableComponent.js';
