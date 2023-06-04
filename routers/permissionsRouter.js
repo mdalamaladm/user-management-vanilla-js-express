c9ab7a26967c076f248a6c90752f1b88856b56aa
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-04 18:52:24 +0700

    feat(permission-router): add reference

--- routers/permissionsRouter.js
+++ routers/permissionsRouter.js
@@ -6,29 +6,40 @@ const { checkAccessMiddleware } = require('../utils/validation.js');
 
 const permissionsRouter = express.Router();
 
-permissionsRouter.use(authJWTMiddleware);
-permissionsRouter.use(checkAccessMiddleware('access-permissions'));
-
-permissionsRouter.route('/')
-  .get(async (req, res) => {
+function getPermissions (internalCode, internalErrorCode) {
+  return async (req, res) => {
     try {
-      const roles =
+      const permissions =
         (await pgPool.query(`SELECT permissions.id, permissions.name FROM permissions`)).rows;
 
       res.status(200).json({
         httpCode: 200,
-        code: 'UM0014',
+        code: internalCode,
         message: 'Permissions Fetched',
-        data: { roles }
+        data: { permissions }
       });
     } catch (e) {
       res.status(500).json({
         httpCode: 500,
-        code: 'UMER0014',
+        code: internalErrorCode,
         message: e
       });
     }
-  })
+  };
+}
+
+permissionsRouter.use(authJWTMiddleware);
+
+permissionsRouter.get(
+  '/references',
+  checkAccessMiddleware(['access-roles-reference-permissions']),
+  getPermissions('UMREF002', 'UMREFER002')
+)
+
+permissionsRouter.use(checkAccessMiddleware('access-permissions'));
+
+permissionsRouter.route('/')
+  .get(getPermissions('UM0014', 'UMER0014'))
 ;
 
 module.exports = permissionsRouter;
