78a95a378bc8a7351c6753c4f8ab7145986c0034
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-04 16:48:18 +0700

    feat(components): add table component

---
+++ public/components/tableComponent.js
@@ -0,0 +1,82 @@
+export function tableComponent (name, option) {
+  const columnsElement = [];
+  for (const name in option.columns) {
+    const column = option.columns[name];
+    
+    columnsElement.push(`
+      <th>${column}</th>
+    `);
+  }
+
+  if (option.edit) {
+    columnsElement.push(`<th></th>`);
+  }
+
+  if (option.remove) {
+    columnsElement.push(`<th></th>`);
+  }
+
+  console.log(option.data);
+
+  const dataElement = [];
+  for (const index in option.data) {
+    const row = option.data[index];
+
+    const rowElement = [];
+
+    for (const name in option.columns) {
+      const cell = row[name];
+
+      rowElement.push(`
+        <td>${cell}</td>
+      `);
+    }
+
+    if (option.edit) {
+      rowElement.push(`
+        <td><button class="${name}-table-edit">Edit</button></td>
+      `)
+    }
+
+    if (option.remove) {
+      rowElement.push(`
+        <td><button class="${name}-table-remove">Remove</button></td>
+      `)
+    }
+
+    dataElement.push(`
+      <tr>${rowElement.join('\n')}</tr>
+    `);
+  }
+
+  return `
+    <table>
+      <thead>
+        <tr>
+          ${columnsElement.join('\n')}
+        </tr>
+      </thead>
+      <tbody>
+        ${dataElement.join('\n')}
+      </tbody>
+    </table>
+  `;
+}
+
+tableComponent.init = ({ name, edit, remove }) => {
+  if (edit) {
+    const editButtons = document.querySelectorAll(`.${name}-table-edit`);
+
+    Array.from(editButtons).forEach((button, index) => {
+      button.onclick = () => { edit(index) }
+    });
+  }
+
+  if (remove) {
+    const removeButtons = document.querySelectorAll(`.${name}-table-remove`);
+
+    Array.from(removeButtons).forEach((button, index) => {
+      button.onclick = () => { remove(index) }
+    });
+  }
+}
