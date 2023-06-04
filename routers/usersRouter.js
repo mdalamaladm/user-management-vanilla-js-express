c31ac094ddb058ad3d0e134148fce74afbd4b45d
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-04 18:36:08 +0700

    feat(user-router): implement post, put, and delete + handle get token when no role_id

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -84,6 +84,16 @@ usersRouter.post('/token',
         });
       }
 
+      const hasRole = user.role_id;
+
+      if (!hasRole) {
+        return res.status(400).json({
+          httpCode: 400,
+          code: 'UMER002',
+          message: 'This account doesn\'t have any access to the application'
+        });
+      }
+
       const payload = { userId: user.id, roleId: user.role_id };
 
       const token = getJWT(payload);
@@ -113,7 +123,7 @@ usersRouter.route('/')
   .get(async (req, res) => {
     try {
       const users =
-        (await pgPool.query(`SELECT users.id, users.name, users.description, users.photo, users.username, users.hash, roles.name AS role FROM users INNER JOIN roles ON users.role_id = roles.id`)).rows;
+        (await pgPool.query(`SELECT users.id, users.name, users.description, users.photo, users.username, users.hash, roles.name AS role_name, roles.id AS role_id FROM users LEFT JOIN roles ON users.role_id = roles.id`)).rows;
 
       res.status(200).json({
         httpCode: 200,
@@ -126,6 +136,102 @@ usersRouter.route('/')
         httpCode: 500,
         code: 'UMER006',
         message: e
+      });
+    }
+  })
+  .post(
+    requiredBodyMiddleware('UMER007', ['name', 'username', 'hash', 'role_id']),
+    async (req, res) => {
+      try {
+        const id = uuidv4();
+
+        let setValues = [];
+        let columnNames = [];
+
+        for (const name in req.body) {
+          columnNames.push(name);
+
+          if (name === 'hash') {
+            setValues.push(`'${await bcrypt.hash(req.body[name], 10)}'`);
+          } else {
+            setValues.push(`'${req.body[name]}'`);
+          }
+        }
+
+        const user =
+          (await pgPool.query(`INSERT INTO users(id, ${columnNames.join(',')}) VALUES('${id}', ${setValues.join(',')});`));
+
+        res.status(200).json({
+          httpCode: 200,
+          code: 'UM007',
+          message: 'User Created',
+          data: { user }
+        });
+      } catch (e) {
+        res.status(500).json({
+          httpCode: 500,
+          code: 'UMER007',
+          message: e.message
+        });
+      }
+    }
+  )
+
+usersRouter.route('/:id')
+  .put(
+    requiredBodyMiddleware('UMER008', ['name', 'username', 'role_id']),
+    async (req, res) => {
+      try {
+        const { id: userId } = req.params;
+
+        if (!req.body.hash) delete req.body.hash;
+
+        let setValues = []
+
+        for (const name in req.body) {
+          if (name === 'hash') {
+            setValues.push(`${name} = '${await bcrypt.hash(req.body[name], 10)}'`);
+          } else {
+            setValues.push(`${name} = '${req.body[name]}'`);
+          }
+        }
+
+        const user =
+          (await pgPool.query(`UPDATE users SET ${setValues.join(',')} WHERE id = '${userId}';`));
+
+        res.status(200).json({
+          httpCode: 200,
+          code: 'UM008',
+          message: 'User Updated',
+          data: { user }
+        });
+      } catch (e) {
+        res.status(500).json({
+          httpCode: 500,
+          code: 'UMER008',
+          message: e.message
+        });
+      }
+    }
+  )
+  .delete(async (req, res) => {
+    try {
+      const { id: userId } = req.params;
+
+      const user =
+        (await pgPool.query(`DELETE FROM users WHERE id = '${userId}'`));
+
+      res.status(200).json({
+        httpCode: 200,
+        code: 'UM009',
+        message: 'User Deleted',
+        data: { user }
+      });
+    } catch (e) {
+      res.status(500).json({
+        httpCode: 500,
+        code: 'UMER009',
+        message: e.message
       });
     }
   })
