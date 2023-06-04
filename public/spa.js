33a01d82231aa876b68a25bda33ba3cee87f0c9a
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-04 18:32:23 +0700

    feat(spa): add global table style

--- public/spa.js
+++ public/spa.js
@@ -8,6 +8,17 @@ setCSS('global', `
     height: 100%;
     min-height: 100%;
   }
+
+  table {
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+  }
+  
+  td,
+  th {
+    border-top: 1px solid black;
+    border-left: 1px solid black;
+  }
 `);
 
 snackbarComponent.init();
