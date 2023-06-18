b2fc7e81a32d26e57a8393c0cedee0144eff2071
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-18 17:59:03 +0700

    fix: rename server to app + remove old app

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT;
+
+const profileRouter = require('./routers/profileRouter.js');
+const usersRouter = require('./routers/usersRouter.js');
+const rolesRouter = require('./routers/rolesRouter.js');
+const permissionsRouter = require('./routers/permissionsRouter.js');
 
 const app = express();
 
-const PORT = process.env.PORT_APP;
-
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Listening to User Management App Server, port ${PORT}`));
+app.use(cors());
+app.use(express.json());
+
+app.use('/profile', profileRouter);
+app.use('/users', usersRouter);
+app.use('/roles', rolesRouter);
+app.use('/permissions', permissionsRouter);
+
+
+app.listen(PORT, () => console.log(`Listening to User Management, port ${PORT}`));
