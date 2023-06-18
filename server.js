e4fae889c1dd3f77299a1a9fe7cd3e103d08487a
mdalamaladm <muhammaddzakyalam@gmail.com>
2023-06-18 17:58:28 +0700

    feat: integrate client and server in one express instance

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT_SERVER;
+const PORT = process.env.PORT;
 
 const profileRouter = require('./routers/profileRouter.js');
 const usersRouter = require('./routers/usersRouter.js');
 const rolesRouter = require('./routers/rolesRouter.js');
 const permissionsRouter = require('./routers/permissionsRouter.js');
 
-const server = express();
+const app = express();
 
-server.use(cors());
-server.use(express.json());
+app.use(express.static('public'));
 
-server.use('/profile', profileRouter);
-server.use('/users', usersRouter);
-server.use('/roles', rolesRouter);
-server.use('/permissions', permissionsRouter);
+app.use(cors());
+app.use(express.json());
+
+app.use('/profile', profileRouter);
+app.use('/users', usersRouter);
+app.use('/roles', rolesRouter);
+app.use('/permissions', permissionsRouter);
 
 
-server.listen(PORT, () => console.log(`Listening to User Management API server, port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening to User Management, port ${PORT}`));
